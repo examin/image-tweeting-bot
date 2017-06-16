2b7e5215565e2d5aac6c51d31d26acb57330853b
manish sharma <manish.sharmaserver008@yahoo.com>
2017-06-16 15:37:11 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'FE87fpsv5O2jYBk7d', 
-  consumer_secret:      'fC49dLuVR71YG86FtIuhww3XnoPgPTfsLjE0o',
-  access_token:         '28641926sNGMFSUsK5lf4NxNRf79mKg3qz',
-  access_token_secret:  'unDBxg3xCn71BgrNXENSlPbN1ouLXU4'
-}
