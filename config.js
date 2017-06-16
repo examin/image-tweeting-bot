5f28d97251328ecbe379968716298fddf3a1e3e9
manish sharma <manish.sharmaserver008@yahoo.com>
2017-06-16 15:36:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'FE87fp4S4rZAfYsv5O2jYBk7d', 
-  consumer_secret:      'fC49dLuVK2ZgIfslkChTBR71YG86FtIuhww3XnoPgPTfsLjE0o',
-  access_token:         '2864192678-OLnOwrdz4Ugw9sNGMFSUsK5lf4NxNRf79mKg3qz',
-  access_token_secret:  'unDBxgAoGtj08RFytRbm3xCn71BgrNXENSlPbN1ouLXU4'
+  consumer_key:         'FE87fpsv5O2jYBk7d', 
+  consumer_secret:      'fC49dLuVR71YG86FtIuhww3XnoPgPTfsLjE0o',
+  access_token:         '28641926sNGMFSUsK5lf4NxNRf79mKg3qz',
+  access_token_secret:  'unDBxg3xCn71BgrNXENSlPbN1ouLXU4'
 }
